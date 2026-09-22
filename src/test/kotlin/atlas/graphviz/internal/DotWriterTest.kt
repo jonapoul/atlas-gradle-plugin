282b4685e6336adf362f4279c607665c17da449f
Jon Poulton <jpoulton@pm.me>
2026-09-22 20:39:16 +0100

    Fix stale expectations in sub-subgraph test

--- src/test/kotlin/atlas/graphviz/internal/DotWriterTest.kt
+++ src/test/kotlin/atlas/graphviz/internal/DotWriterTest.kt
@@ -107,8 +107,8 @@ internal class DotWriterTest {
           ":app"
           subgraph cluster_data {
             label = ":data"
-            ":data:a"
-            ":data:b"
+            ":data:a" [label=":a"]
+            ":data:b" [label=":b"]
             subgraph cluster_sub {
               label = ":sub"
               ":data:sub:sub1" [label=":sub1"]
@@ -117,14 +117,14 @@ internal class DotWriterTest {
           }
           subgraph cluster_domain {
             label = ":domain"
-            ":domain:a"
-            ":domain:b"
+            ":domain:a" [label=":a"]
+            ":domain:b" [label=":b"]
           }
           subgraph cluster_ui {
             label = ":ui"
-            ":ui:a"
-            ":ui:b"
-            ":ui:c"
+            ":ui:a" [label=":a"]
+            ":ui:b" [label=":b"]
+            ":ui:c" [label=":c"]
           }
           ":app" -> ":ui:a"
           ":app" -> ":ui:b"
